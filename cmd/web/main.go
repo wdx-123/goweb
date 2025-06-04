f71f7080e134ca0cb20d850fd20663269b4bc461
wang <wang@123.com>
2025-06-04 17:39:56 +0800

    third commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"GoWeb/internal/web-app/controller"
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	fmt.Println("项目开始喽")
+	// 注册路由：
+	http.HandleFunc("/login", controller.LoginHandler)
+	http.HandleFunc("/register", controller.RegisterHandler)
+
+	// 静态资源加载
+	fs := http.FileServer(http.Dir("D:\\workspace_go\\GoWeb\\web\\static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	// 开始监听
+	http.ListenAndServe(":8888", nil)
 }
